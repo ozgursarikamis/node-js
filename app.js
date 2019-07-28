49c24c34f44ace5e7a918d8ca10b6a1e8d919ca2
ozgursarikamis <ozgursarikamis@gmail.com>
2019-07-28 15:12:10 +0300

    fields consola yazıldı

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ http.createServer(function (req, res) {
     if (req.url == '/fileupload') {
       var form = new formidable.IncomingForm();
       form.parse(req, function (err, fields, files) {
+        console.log(fields);        
         var oldpath = files.filetoupload.path;
         var newpath = 'C:/Users/OZGURSARIKAMIS/Desktop/' + files.filetoupload.name;
         fs.rename(oldpath, newpath, function (err) {
