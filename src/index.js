611445c7c5f49387da917cb9b4182c1adff53486
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-08 13:43:10 +0300

    slice function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-const grab = flag => {
-    let inedxAfterFlag = process.argv.indexOf(flag) + 1;
-    return process.argv[inedxAfterFlag];
-}
-const greeting = grab("--greeting");
-const user = grab("--user");
-console.log({greeting, user });
+console.log(process.argv);
+const args = process.argv.slice(2);
+console.log('args :', args);
+
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+console.log('fruits :', fruits);
+console.log('fruits.slice(1) :', fruits.slice(1));
+console.log('fruits.slice(2) :', fruits.slice(2));
+console.log('fruits.slice(3) :', fruits.slice(3));
+console.log('fruits.slice(4) :', fruits.slice(4));
+console.log('fruits.slice(5) :', fruits.slice(5)); 
+
+console.log('1den 3e kadar : fruits.slice(1,3) :', fruits.slice(1,3));
+console.log('2den 4e kadar : fruits.slice(2,4) :', fruits.slice(2,4)); // 
