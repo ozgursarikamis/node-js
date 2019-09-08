413d86c41f38dee0d32e86f27a1e44e686f13492
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-09 00:12:32 +0300

    link reference
    https://medium.com/the-guild/getting-to-know-nodes-child-process-module-8ed63038f3fa

--- child.process.js
+++ child.process.js
@@ -7,6 +7,15 @@
 // and we can use it to pass that command any arguments. 
 
 const { spawn } = require('child_process');
-spawn('git', ['log']).stdout.pipe(process.stdout); 
+
+spawn('git', ['log'], {  
+        stdio: 'inherit' // Will use process .stdout, .stdin, .stderr
+    })
+    //.stdout.pipe(process.stdout); 
 // 1st argument: executable file path 
 // 2nd argument: arguments
+
+// Note that every child_process function 
+// will also have a “sync” version of it (e.g. spawnSync())
+
+// https://medium.com/the-guild/getting-to-know-nodes-child-process-module-8ed63038f3fa
