e5a94e2ba31fa998f73a5aa78eeb03ff6de50a89
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-08 13:48:14 +0300

    custom grab function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-console.log(process.argv);
-const args = process.argv.slice(2);
-console.log('args :', args);
+// console.log(process.argv);
+const grab = flag => {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
+}
 
-var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-console.log('fruits :', fruits);
-console.log('fruits.slice(1) :', fruits.slice(1));
-console.log('fruits.slice(2) :', fruits.slice(2));
-console.log('fruits.slice(3) :', fruits.slice(3));
-console.log('fruits.slice(4) :', fruits.slice(4));
-console.log('fruits.slice(5) :', fruits.slice(5)); 
+const greeting = grab("--greeting");
+const user = grab("--user");
 
-console.log('1den 3e kadar : fruits.slice(1,3) :', fruits.slice(1,3));
-console.log('2den 4e kadar : fruits.slice(2,4) :', fruits.slice(2,4)); // 
+console.log({greeting, user});
