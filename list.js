fbb0f52c555e806a91d872eebce659c09e6251ca
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-08 19:21:24 +0300

    read directory (async)

--- list.js
+++ list.js
@@ -4,3 +4,15 @@ const files = fs.readdirSync("./");
 console.log("================= started reading ========================");
 files.forEach(item => console.log('item :', item));
 console.log("================= ended reading ==========================");
+
+
+// reading files asynchronously:
+console.log("================= started reading (async) ========================");
+fs.readdir("./", (error, files) => {
+    if(error) {
+        throw error;
+    }
+    console.log("complete");
+    console.log('files :', files);
+});
+console.log("================= ended reading (async) ========================");
