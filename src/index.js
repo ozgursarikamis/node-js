228165d96c50de86000ebab5af954e6f0576f9e1
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-08 15:01:04 +0300

    core modules path

--- src/index.js
+++ src/index.js
@@ -1,35 +1,4 @@
-//process.stdout.write("hello");
+const path = require("path");
 
-const questions = [
-    "What is your name",
-    "What would you rather be doing",
-    "What is your preferred programming language"
-];
-
-const ask = (i = 0) => {
-    process.stdout.write(`\n\n\n ${questions[i]}`);
-    process.stdout.write(' > ');
-}
-
-ask();
-
-const answers = [];
-process.stdin.on('data', data => {
-    // process.stdout.write(`\n\n ${data.toString().trim()} \n\n`)
-    answers.push(data.toString().trim());
-    if(answers.length < questions.length) {
-        ask(answers.length);
-    } else {
-        process.exit();
-    }
-});
-
-process.on("exit", () => {
-    const [name, activity, language] = answers;
-    console.log(`
-
-    Thank you for your answers.
-
-    Go ${activity} ${name} you can write ${language} code later!!!!
-    `);    
-})
+const dirUploads = path.join(__dirname, "www", "file", "uploads");
+console.log('dirUploads :', dirUploads);
