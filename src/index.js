a882656eb9058f87d5222e947115de35bc23f058
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-08 15:08:27 +0300

    core modules util and v8

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const util = require("util");
+const v8 = require("v8");
 
-const dirUploads = path.join(__dirname, "www", "file", "uploads");
-console.log('dirUploads :', dirUploads);
+util.log(path.basename(__filename));
+util.log("this is a util log");
+util.log(v8.getHeapStatistics());
