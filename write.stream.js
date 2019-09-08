b10f7f0af706fc9427d3b748d827965ef7af5cd9
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-08 23:46:23 +0300

    createReadStream

--- write.stream.js
+++ write.stream.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
 
 const file = "./assets/file.txt";
-const reader = fs.createReadStream(file, { highWaterMark: 8, start: 8, end: 64});
+const reader = fs.createReadStream(file, { });
 
 reader.on("data", chunk => {
     console.log('chunk.toString() :', chunk.toString());
+});
+
+reader.pipe(fs.createWriteStream("./assets/file_write.txt"))
+    .on("data", chunk => {
+        console.log('chunk.toString() :', chunk.toString());
 });
