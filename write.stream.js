5224de0c43fc4479ee2a9b8e2a09f18eceef4123
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-08 23:51:05 +0300

    streaming close event

--- write.stream.js
+++ write.stream.js
@@ -1,13 +1,23 @@
 const fs = require("fs");
 
 const file = "./assets/file.txt";
-const reader = fs.createReadStream(file, { });
+const reader = fs.createReadStream(file);
 
 reader.on("data", chunk => {
     console.log('chunk.toString() :', chunk.toString());
 });
 
 reader.pipe(fs.createWriteStream("./assets/file_write.txt"))
-    .on("data", chunk => {
+        .on("data", chunk => {
         console.log('chunk.toString() :', chunk.toString());
-});
+        })
+        .on("error", error => {
+            console.error(error);
+            throw error;            
+    })
+
+// The close event is called once the read stream is all over
+.on("close", () => {
+    console.warn("warning: on.close event");
+    console.info("info: on.close event");
+})
