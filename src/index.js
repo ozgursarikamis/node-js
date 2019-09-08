0d39bfe4b632df6a80f372824eb51bd063d86319
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-08 14:14:53 +0300

    yargs package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-// console.log(process.argv);
-const grab = flag => {
-    let indexAfterFlag = process.argv.indexOf(flag) + 1;
-    return process.argv[indexAfterFlag];
-}
+const yargs = require("yargs");
 
-const greeting = grab("--greeting");
-const user = grab("--user");
+const argv = yargs
+    .command("lyr", "Tells whether a year is leap or not", {
+        year: {
+            description: "the year to check for",
+            alias: "y",
+            type: "number"
+        }
+    })
+    .option("time", {
+        alias: "t",
+        description: "tell the present year",
+        type: "boolean"
+    })
+    .help()
+    .alias("help", "h").argv;
 
-console.log({greeting, user});
+    if(argv.time) {
+        console.log(`The current time is ${new Date().toLocaleTimeString()}`);        
+    }
+    if(argv._.includes("lyr")) {
+        const year = argv.year || new Date().getFullYear();
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+            console.log(`${year} is a Leap Year`);
+        } else {
+            console.log(`${year} is NOT a Leap Year`);
+        }
+    }
+    console.log(argv);
