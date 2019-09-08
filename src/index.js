b567f8747fb5096d467f027209414d0da279ab55
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-09-08 15:10:31 +0300

    core modules require only what you need

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
-const util = require("util");
-const v8 = require("v8");
+const { log } = require("util");
+const { getHeapStatistics } = require("v8");
 
-util.log(path.basename(__filename));
-util.log("this is a util log");
-util.log(v8.getHeapStatistics());
+log(path.basename(__filename));
+log("this is a util log");
+log(getHeapStatistics());
