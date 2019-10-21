f885978f44552d149766f07e2c98a9c6eb9e3f01
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-10-21 23:23:51 +0300

    setTimeout, setImmediate, nextTick

--- src/modules/set-immediate.js
+++ src/modules/set-immediate.js
@@ -5,4 +5,25 @@ but as soon as possible, one option is to use the setImmediate()
 
 setImmediate(() => {
     console.warn("setImmediate");
-})
+});
+
+let racer = () => {
+    setTimeout(() => { console.log("timeout") }, 0);
+    setImmediate(() => { console.log("immediate"); });
+    process.nextTick(() => { console.log("nextTick"); });
+    console.log("current event loop");
+}
+
+// The first one executed was process.nextTick,
+// which puts its callback at the front of the event queue. 
+// It will execute after the code currently being executed but
+// before any I/O events or timers.
+
+// Next is "timeout". Since we passed setTimeout a timeout of 0, 
+// there's no additional enforced delay before its execution, 
+// and it is placed on into the timer queue during the next loop.
+
+// Since the check queue occurs later than the timer queue,
+// setImmediate will be slower than setTimeout 0.
+
+racer();
